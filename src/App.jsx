a7c6280e6fa441a8940b624cf45cb627cd8e126a
Amit @Hostologies <d3v@hostologies.com>
2025-03-17 21:58:20 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/v1/register', {
+      const response = await fetch('https://ecommerce-backend-client.vercel.app/api/v1/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
