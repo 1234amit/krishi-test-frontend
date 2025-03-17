c90901abc3a40d186aa83e57c7f03d6a798a2c9e
Amit @Hostologies <d3v@hostologies.com>
2025-03-17 23:40:13 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://ecommerce-backend-client.vercel.app/api/v1/register', {
+      const response = await fetch('https://ecommerce-client-backend.onrender.com/api/v1/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
